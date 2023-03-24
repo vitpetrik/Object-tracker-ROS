0036c7a5dd4aada5c63939187dc706ba079cf91d
Vit Petrik <petrivi2@fel.cvut.cz>
2023-03-25 00:17:27 +0100

    Refactored some steps

--- include/tracker.h
+++ include/tracker.h
@@ -20,45 +20,17 @@
 
 #include <Eigen/Dense>
 
-#define STATES_NUM 18
-
-enum TRANSITION_MODEL_TYPE
-{
-    CONSTANT_POSITION,
-    CONSTANT_VELOCITY,
-    CONSTANT_ACCELERATION
-};
-
-enum class STATE
-{
-    X,
-    X_dt,
-    X_dt2,
-    Y,
-    Y_dt,
-    Y_dt2,
-    Z,
-    Z_dt,
-    Z_dt2,
-    ROLL,
-    ROLL_dt,
-    ROLL_dt2,
-    PITCH,
-    PITCH_dt,
-    PITCH_dt2,
-    YAW,
-    YAW_dt,
-    YAW_dt2
-};
+#include "helperfun.h"
 
 namespace kalman
 {
-    using range_ukf_t = mrs_lib::UKF<STATES_NUM, 0, 1>;
-    using pose_lkf_t = mrs_lib::LKF<STATES_NUM, 0, 6>;
+    using range_ukf_t = mrs_lib::UKF<(int)STATE::STATES_NUM, 0, 1>;
+    using pose_lkf_t = mrs_lib::LKF<(int)STATE::STATES_NUM, 0, 6>;
+    using predict_lkf_t = mrs_lib::LKF<(int)STATE::STATES_NUM, 0, 6>;
 
-    using A_t = pose_lkf_t::A_t;
-    using P_t = pose_lkf_t::P_t;
-    using x_t = pose_lkf_t::x_t;
+    using A_t = predict_lkf_t::A_t;
+    using P_t = predict_lkf_t::P_t;
+    using x_t = predict_lkf_t::x_t;
 }
 
 /**
@@ -76,9 +48,9 @@ public:
 
     std::pair<kalman::x_t, kalman::P_t> predict(ros::Time, bool apply_update = true);
 
-    std::pair<kalman::x_t, kalman::P_t> correctPose(ros::Time, kalman::pose_lkf_t::z_t, kalman::pose_lkf_t::R_t, bool apply_update = true);
+    std::pair<kalman::pose_lkf_t::x_t, kalman::pose_lkf_t::P_t> correctPose(ros::Time, kalman::pose_lkf_t::z_t, kalman::pose_lkf_t::R_t, bool apply_update = true);
 
-    std::pair<kalman::x_t, kalman::P_t> correctRange(ros::Time, kalman::range_ukf_t::z_t, kalman::range_ukf_t::R_t, bool apply_update = true);
+    std::pair<kalman::range_ukf_t::x_t, kalman::range_ukf_t::P_t> correctRange(ros::Time, kalman::range_ukf_t::z_t, kalman::range_ukf_t::R_t, bool apply_update = true);
 
     const auto get_last_correction() const { return last_correction; }
     const auto get_update_count() const { return update_count; }
@@ -96,12 +68,10 @@ private:
     kalman::x_t state_vector;
     kalman::P_t covariance;
 
-    kalman::pose_lkf_t pose_lkf;
-    kalman::A_t A_matrix;
-    kalman::pose_lkf_t::B_t B_matrix;
-    kalman::pose_lkf_t::H_t H_matrix;
-
+    kalman::predict_lkf_t predict_lkf;
     kalman::range_ukf_t range_ukf;
+    kalman::pose_lkf_t pose_lkf;
+
 
     ros::Time last_prediction;
     ros::Time last_correction;
--- src/tracker.cpp
+++ src/tracker.cpp
@@ -20,53 +20,43 @@ kalman::range_ukf_t::x_t transition_ukf(const kalman::range_ukf_t::x_t &x, const
 
 kalman::range_ukf_t::z_t observe_ukf(const kalman::range_ukf_t::x_t &x)
 {
-    kalman::range_ukf_t::z_t z = kalman::range_ukf_t::z_t::Zero();
+    Eigen::VectorXd pose(3);
 
-    z << std::sqrt(x[(int)STATE::X] * x[(int)STATE::X] + x[(int)STATE::Y] * x[(int)STATE::Y] + x[(int)STATE::Z] * x[(int)STATE::Z]);
+    pose << x[(int)STATE::X], x[(int)STATE::Y], x[(int)STATE::Z];
+    kalman::range_ukf_t::z_t z;
 
+    z << pose.norm();
     return z;
 }
 
 Eigen::MatrixXd modelMatrix(double dt, int model_type)
 {
+    Eigen::Matrix3d model_block = Eigen::Matrix3d::Zero();
     Eigen::MatrixXd model(9, 9);
 
     switch (model_type)
     {
     case TRANSITION_MODEL_TYPE::CONSTANT_POSITION:
-        model << 1, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 1, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 1, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0;
+        model_block << 1, 0, 0,
+                        0, 0, 0,
+                        0, 0, 0;
+
         break;
     case TRANSITION_MODEL_TYPE::CONSTANT_VELOCITY:
-        model << 1, dt, 0, 0, 0, 0, 0, 0, 0,
-            0, 1, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 1, dt, 0, 0, 0, 0,
-            0, 0, 0, 0, 1, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 1, dt, 0,
-            0, 0, 0, 0, 0, 0, 0, 1, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0;
+        model_block << 1, dt, 0,
+                        0, 1, 0,
+                        0, 0, 0;
         break;
     case TRANSITION_MODEL_TYPE::CONSTANT_ACCELERATION:
-        model << 1, dt, 0.5 * dt * dt, 0, 0, 0, 0, 0, 0,
-            0, 1, dt, 0, 0, 0, 0, 0, 0,
-            0, 0, 1, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 1, dt, 0.5 * dt * dt, 0, 0, 0,
-            0, 0, 0, 0, 1, dt, 0, 0, 0,
-            0, 0, 0, 0, 0, 1, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 1, dt, 0.5 * dt * dt,
-            0, 0, 0, 0, 0, 0, 0, 1, dt,
-            0, 0, 0, 0, 0, 0, 0, 0, 1;
+        model_block << 1, dt, 0.5*pow(dt, 2),
+                0, 1, dt,
+                0, 0, 1;
         break;
     }
+
+    model.block<3, 3>(0, 0) = model_block;
+    model.block<3, 3>(3, 3) = model_block;
+    model.block<3, 3>(6, 6) = model_block;
 
     return model;
 }
@@ -112,9 +102,9 @@ kalman::A_t transitionMatrix(double dt, int position_model, int rotation_model)
     return A;
 }
 
-kalman::pose_lkf_t::Q_t processNoiseMatrix(double dt, int position_model, int rotation_model, double spectral_density_pose, double spectral_density_rotation)
+kalman::predict_lkf_t::Q_t processNoiseMatrix(double dt, int position_model, int rotation_model, double spectral_density_pose, double spectral_density_rotation)
 {
-    kalman::pose_lkf_t::Q_t Q = kalman::pose_lkf_t::Q_t::Zero();
+    kalman::predict_lkf_t::Q_t Q = kalman::predict_lkf_t::Q_t::Zero();
 
     Q.topLeftCorner(9, 9) = Q_continuous_white_noise(position_model, dt, spectral_density_pose);
     Q.bottomRightCorner(9, 9) = Q_continuous_white_noise(rotation_model, dt, spectral_density_rotation);
@@ -151,35 +141,33 @@ Tracker::Tracker(kalman::pose_lkf_t::z_t z, kalman::pose_lkf_t::R_t R, int posit
     this->covariance((int)STATE::X, (int)STATE::X) = R(0, 0);
     this->covariance((int)STATE::X, (int)STATE::Y) = R(0, 1);
     this->covariance((int)STATE::X, (int)STATE::Z) = R(0, 2);
-    this->covariance((int)STATE::Y, (int)STATE::X) = R(1, 0);
     this->covariance((int)STATE::Y, (int)STATE::Y) = R(1, 1);
     this->covariance((int)STATE::Y, (int)STATE::Z) = R(1, 2);
-    this->covariance((int)STATE::Z, (int)STATE::X) = R(2, 0);
-    this->covariance((int)STATE::Z, (int)STATE::Y) = R(2, 1);
     this->covariance((int)STATE::Z, (int)STATE::Z) = R(2, 2);
 
     this->covariance((int)STATE::ROLL, (int)STATE::ROLL) = R(3, 3);
     this->covariance((int)STATE::ROLL, (int)STATE::PITCH) = R(3, 4);
     this->covariance((int)STATE::ROLL, (int)STATE::YAW) = R(3, 5);
-    this->covariance((int)STATE::PITCH, (int)STATE::ROLL) = R(4, 3);
     this->covariance((int)STATE::PITCH, (int)STATE::PITCH) = R(4, 4);
     this->covariance((int)STATE::PITCH, (int)STATE::YAW) = R(4, 5);
-    this->covariance((int)STATE::YAW, (int)STATE::ROLL) = R(5, 3);
-    this->covariance((int)STATE::YAW, (int)STATE::PITCH) = R(5, 4);
     this->covariance((int)STATE::YAW, (int)STATE::YAW) = R(5, 5);
 
-    this->A_matrix = kalman::A_t::Zero();
-    this->B_matrix = kalman::pose_lkf_t::B_t::Zero();
-    this->H_matrix = kalman::pose_lkf_t::H_t::Zero();
+    this->covariance.triangularView<Eigen::Lower>() = this->covariance.transpose();
+    kalman::pose_lkf_t::H_t H_matrix = kalman::pose_lkf_t::H_t::Zero();
 
-    this->H_matrix(0, (int)STATE::X) = 1;
-    this->H_matrix(1, (int)STATE::Y) = 1;
-    this->H_matrix(2, (int)STATE::Z) = 1;
-    this->H_matrix(3, (int)STATE::ROLL) = 1;
-    this->H_matrix(4, (int)STATE::PITCH) = 1;
-    this->H_matrix(5, (int)STATE::YAW) = 1;
+    H_matrix(0, (int)STATE::X) = 1;
+    H_matrix(1, (int)STATE::Y) = 1;
+    H_matrix(2, (int)STATE::Z) = 1;
+    H_matrix(3, (int)STATE::ROLL) = 1;
+    H_matrix(4, (int)STATE::PITCH) = 1;
+    H_matrix(5, (int)STATE::YAW) = 1;
 
-    this->pose_lkf = kalman::pose_lkf_t(this->A_matrix, this->B_matrix, this->H_matrix);
+    this->pose_lkf = kalman::pose_lkf_t(kalman::pose_lkf_t::A_t::Identity(),
+                                        kalman::pose_lkf_t::B_t::Zero(),
+                                        H_matrix);
+    this->predict_lkf = kalman::predict_lkf_t(kalman::predict_lkf_t::A_t::Identity(),
+                                              kalman::predict_lkf_t::B_t::Zero(),
+                                              kalman::predict_lkf_t::H_t::Identity());
     this->range_ukf = kalman::range_ukf_t(transition_ukf, observe_ukf);
 
     this->last_prediction = ros::Time::now();
@@ -199,10 +187,10 @@ std::pair<kalman::x_t, kalman::P_t> Tracker::predict(ros::Time time, bool apply_
 
     double dt = std::fmax(std::fmin((time - this->last_prediction).toSec(), (time - this->last_correction).toSec()), 0.0);
 
-    this->pose_lkf.A = transitionMatrix(dt, this->position_model_type, this->rotation_model_type);
-    kalman::pose_lkf_t::statecov_t statecov = {x, P, time};
+    this->predict_lkf.A = transitionMatrix(dt, this->position_model_type, this->rotation_model_type);
+    kalman::predict_lkf_t::statecov_t statecov = {x, P, time};
 
-    statecov = this->pose_lkf.predict(statecov, kalman::pose_lkf_t::u_t::Zero(), processNoiseMatrix(dt, this->position_model_type, this->rotation_model_type, this->spectral_density_pose, this->spectral_density_rotation), dt);
+    statecov = this->predict_lkf.predict(statecov, kalman::predict_lkf_t::u_t::Zero(), processNoiseMatrix(dt, this->position_model_type, this->rotation_model_type, this->spectral_density_pose, this->spectral_density_rotation), dt);
     x = statecov.x;
     P = statecov.P;
 
@@ -217,32 +205,20 @@ std::pair<kalman::x_t, kalman::P_t> Tracker::predict(ros::Time time, bool apply_
     return std::make_pair(x, P);
 }
 
-std::pair<kalman::x_t, kalman::P_t> Tracker::correctPose(ros::Time time, kalman::pose_lkf_t::z_t z, kalman::pose_lkf_t::R_t R, bool apply_update)
+std::pair<kalman::pose_lkf_t::x_t, kalman::pose_lkf_t::P_t> Tracker::correctPose(ros::Time time, kalman::pose_lkf_t::z_t z, kalman::pose_lkf_t::R_t R, bool apply_update)
 {
-    kalman::x_t x = this->state_vector;
-    kalman::P_t P = this->covariance;
+    kalman::pose_lkf_t::x_t x = this->state_vector;
+    kalman::pose_lkf_t::P_t P = this->covariance;
 
     x[(int)STATE::ROLL] = fixAngle(x[(int)STATE::ROLL], z[3]);
     x[(int)STATE::PITCH] = fixAngle(x[(int)STATE::PITCH], z[4]);
     x[(int)STATE::YAW] = fixAngle(x[(int)STATE::YAW], z[5]);
 
-    double match_level_pos = gaussJointMaxVal(
-        R.topLeftCorner(3, 3),
-        P.topLeftCorner(3, 3),
-        z.topRows(3),
-        x.topRows(3));
-
-    auto R_temp = R;
-    // R_temp.topRightCorner(3, 3).setZero();
-    // R_temp.bottomLeftCorner(3, 3).setZero();
-
-    // R_temp.topLeftCorner(3, 3) *= (1.0 / (match_level_pos));
-
     kalman::pose_lkf_t::statecov_t statecov = {x, P, time};
 
     try
     {
-        statecov = this->pose_lkf.correct(statecov, z, R_temp);
+        statecov = this->pose_lkf.correct(statecov, z, R);
     }
     catch ([[maybe_unused]] std::exception &e)
     {
@@ -252,11 +228,6 @@ std::pair<kalman::x_t, kalman::P_t> Tracker::correctPose(ros::Time time, kalman:
 
     x = statecov.x;
     P = statecov.P;
-
-    // auto eigens_pos = P.topLeftCorner(9, 9).eigenvalues();
-    // double min_eig_pos = eigens_pos.real().minCoeff();
-    // auto eigens_rot = P.bottomRightCorner(9, 9).eigenvalues();
-    // P.topLeftCorner(9, 9) += Eigen::MatrixXd::Identity(9, 9) * (min_eig_pos * (match_level_pos));
 
     x[(int)STATE::ROLL] = fixAngle(x[(int)STATE::ROLL], 0);
     x[(int)STATE::PITCH] = fixAngle(x[(int)STATE::PITCH], 0);
@@ -274,10 +245,10 @@ std::pair<kalman::x_t, kalman::P_t> Tracker::correctPose(ros::Time time, kalman:
     return std::make_pair(x, P);
 }
 
-std::pair<kalman::x_t, kalman::P_t> Tracker::correctRange(ros::Time time, kalman::range_ukf_t::z_t z, kalman::range_ukf_t::R_t R, bool apply_update)
+std::pair<kalman::range_ukf_t::x_t, kalman::range_ukf_t::P_t> Tracker::correctRange(ros::Time time, kalman::range_ukf_t::z_t z, kalman::range_ukf_t::R_t R, bool apply_update)
 {
-    kalman::x_t x = this->state_vector;
-    kalman::P_t P = this->covariance;
+    kalman::range_ukf_t::x_t x = this->state_vector;
+    kalman::range_ukf_t::P_t P = this->covariance;
 
     kalman::range_ukf_t::statecov_t statecov = {x, P, time};
 
